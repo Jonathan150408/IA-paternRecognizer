59db96ae09d272c4d3e637e57e82ebf640ef85bd
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-01-16 10:28:34 +0100

    chore(main program) : readding the program
    [0][Done]
    Did this because the gitignore was added later.

---
+++ IA03/Layer.cs
@@ -0,0 +1,67 @@
+﻿using IA03;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA03
+{
+    internal class Layer
+    {
+		/// <summary>
+		/// Enum's constructor
+		/// </summary>
+		public enum Function
+		{
+			sigmoid,
+			tanh,
+			abs_sum
+		}
+		/// <summary>
+		/// Represents the function of the layer
+		/// </summary>
+		private Function _function;
+		public Function function
+		{
+			get { return _function; }
+			private set { _function = value; }
+		}
+		/// <summary>
+		/// The neurons of the layer
+		/// </summary>
+		private List<Neuron> _neurons;
+		public List<Neuron> Neurons
+		{
+			get { return _neurons; }
+			private set { _neurons = value; }
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="neurons"></param>
+		public Layer(List<Neuron> neurons, Function func)
+		{
+			Neurons = neurons;
+			function = func;
+		}
+		/// <summary>
+		/// Calculate the result of the layer
+		/// </summary>
+		/// <param name="inputs">The inputs of the layer</param>
+		/// <returns>An array where each neuron has a place</returns>
+		public double[] GetLayerResults(List<double> inputs)
+		{
+			double[] results = new double[this.Neurons.Count];
+			int index = 0;
+			foreach (Neuron neuron in this.Neurons)
+			{
+				results[index] = neuron.GetResult(inputs, this.function);
+				index++;
+			}
+			return results;
+		}
+
+	}
+}
---
+++ IA03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IA03
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            IA ia = new IA();
+            Application.Run(ia);
+        }
+    }
+}
