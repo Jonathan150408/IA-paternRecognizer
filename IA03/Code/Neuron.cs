7456da9e114c8df53ef1a303199c741ed8e7b753
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-03-06 15:31:38 +0100

    feat(entrainement) : finaliser l'entrainement
    C'est solide, 90% accurate

--- IA03/Code/Neuron.cs
+++ IA03/Code/Neuron.cs
@@ -99,7 +99,7 @@ namespace IA03
         public (double[], double) CorrectNeuron(double expected, double output, List<double> processed_values, Layer.Function function)
         {
             //learning rate
-            const double LEARNING_RATE = 0.005;
+            const double LEARNING_RATE = 0.002;
 
             //delta
             double delta;
