9944a5f384734fffbff65cc3a26368de6d0a94dc
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-04-10 15:13:05 +0200

    feat(nouvelle version) : créer un v4 et implémenter la structure de base
    Je compte rendre le programme plus adapté à une utilisation générale, j'aimerais que le programme puisse faire tourner différents réseau en se basant sur du JSON.
    
    Structure proposée :
    1 solution -> 3 projets
    - Console : gestion de l'output (et input textuelle pour plus tard si besoin)
    - Core : gestion du réseau et logique métier
    - Form : gestion des input visuelles

---
+++ IA04/Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace IAConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //boucle qui permet de recommencer/relancer le programme
+            ConsoleKey keyUserInput;
+            do
+            {
+                //charger les data -> Core
+                //si besoin, lancer un form -> Form
+                //calculer la réponse -> Core
+                //afficher la réponse -> Console
+                //demander la correction -> Console
+                //corriger -> Core
+                //recalculer la réponse -> Core
+                //afficher la réponse -> Console
+
+                //demander si souhaite recommencer
+                Console.WriteLine("Souhaitez vous recommencer ?");
+                keyUserInput = Console.ReadKey(true).Key;
+            } while (keyUserInput != ConsoleKey.Escape && keyUserInput != ConsoleKey.N);
+
+            //quitter le programme
+            Console.WriteLine("\nMerci d'avoir testé ce programme \\^o^/\nAppuyez sur une touche pour fermer le programme...");
+            Console.Read();
+            Environment.Exit(0);
+        }
+    }
+}
