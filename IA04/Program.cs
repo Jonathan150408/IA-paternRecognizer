18463c9fce37efaafe90ab2717502738ecc4b554
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-06-05 15:21:50 +0200

    feat(programme) : ajouter des intructions

--- IA04/Program.cs
+++ IA04/Program.cs
@@ -2,6 +2,7 @@
 using IA04.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,42 @@ namespace IA04
 {
     internal class Program
     {
+        /// <summary>
+        /// A chrono to measure time elapsing and thus have stats
+        /// </summary>
+        static Stopwatch chrono = new Stopwatch();
+
+        /// <summary>
+        /// The MAIN
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // Start the chrono
+            chrono.Start();
+
+            // Initialize the IA
             IAService dataService = new IAService();
-            Console.WriteLine(dataService.GetPath());
             Network network = dataService.LoadNetwork();
-            Console.ReadLine();
+
+            // Log the chrono value
+            LogChrono("Réseau chargé en ");
+
+            
+        }
+
+        /// <summary>
+        /// Log the chrono value with a custom message
+        /// </summary>
+        /// <param name="message"></param>
+        static void LogChrono(string message)
+        {
+            chrono.Stop();
+            Console.Write(message);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(chrono.ElapsedMilliseconds);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" ms");
         }
     }
 }
