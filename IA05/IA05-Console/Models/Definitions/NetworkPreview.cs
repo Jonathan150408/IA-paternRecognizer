14e671b532ae9bfbbad7844cd2a355fc08835d2c
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-06-08 11:10:19 +0200

    feat(classe networkpreview) : créer une nouvelle classe

---
+++ IA05/IA05-Console/Models/Definitions/NetworkPreview.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IA05_Console.Models.Definitions
+{
+    /// <summary>
+    /// NetworkPreview : A light object that resumes the infos of a neural network without having to load it.
+    /// </summary>
+    public class NetworkPreview
+    {
+        /// <summary>
+        /// Id : An unique positve integer that referes to an object.
+        /// </summary>
+        public uint Id { get; set; }
+        /// <summary>
+        /// Name : A string that is the name of the network.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Description : A string that describes what the IA does.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Path : A string that specifies the path of the file starting from the current folder (/Ressources).
+        /// </summary>
+        public string Path { get; set; }
+        /// <summary>
+        /// NeedForm : A boolean used to know if a form is needed.
+        /// </summary>
+        public bool NeedForm { get; set; }
+        /// <summary>
+        /// GridDimensions : 2 positives intergers that gives the drawing grid's dimensions. FACULTATIVE
+        /// </summary>
+        public (uint, uint)? GridDimensions { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public NetworkPreview(uint id, string name, string description, string path, bool needForm, (uint, uint)? gridDimensions)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Path = path;
+            NeedForm = needForm;
+            GridDimensions = gridDimensions;
+        }
+    }
+}
