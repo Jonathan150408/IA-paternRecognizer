198affcbcd5f5e13badb9226cd649318935e03db
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-06-08 11:15:14 +0200

    feat(previewservice) : créer le service

---
+++ IA05/IA05-Console/Services/PreviewService.cs
@@ -0,0 +1,51 @@
+﻿using IA04.Models;
+using IA05_Console.Models.Definitions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IA05_Console.Services
+{
+    /// <summary>
+    /// PreviewService : A service that loads the previews.
+    /// </summary>
+    public class PreviewService
+    {
+        /// <summary>
+        /// Path : A string that specifies the folder path to take to find the network's file.
+        /// </summary>
+        public string Path { get; private set; }
+
+        /// <summary>
+        /// Contructor
+        /// </summary>
+        public PreviewService()
+        {
+            Path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Ressources", "config.json");
+        }
+
+        /// <summary>
+        /// Permet de créer et charger une IA à partir de JSON
+        /// </summary>
+        /// <returns>Le réseau neuronal</returns>
+        public List<NetworkPreview> LoadNetwork()
+        {
+            string jsonString = File.ReadAllText(Path);
+            List<NetworkPreview> previews = JsonSerializer.Deserialize<List<NetworkPreview>>(jsonString);
+            return previews;
+        }
+
+        /// <summary>
+        /// Gives the used path
+        /// </summary>
+        /// <returns></returns>
+        public string GetPath()
+        {
+            return this.Path;
+        }
+    }
+}
