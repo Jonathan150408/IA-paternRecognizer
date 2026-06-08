3ab2ca188001fd368d203352783e387a9b305e85
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-06-08 11:10:40 +0200

    feat(menu) : implémenter le menu interactif

--- IA05/IA05-Console/Program.cs
+++ IA05/IA05-Console/Program.cs
@@ -37,9 +37,66 @@ namespace IA05_Console
                 }
             }
 
-            
+            // 4. 
 
             Console.Read();
+        }
+
+        /// <summary>
+        /// Displays an interactive menu.
+        /// </summary>
+        /// <param name="title">A facultative title to show</param>
+        /// <param name="choices">A list of choices</param>
+        /// <param name="topLine">The top line of the menu</param>
+        /// <returns>The index of the choice selected in the list.</returns>
+        static int DisplaySelectMenu(string title, List<string> choices, int topLine)
+        {
+            // 1. Set up variables
+            int userChoice = 1;
+            ConsoleKeyInfo userKey;
+            Console.CursorVisible = false;
+            Console.CursorTop = topLine;
+
+            // 2. Writes the choices
+            Console.WriteLine("   " + title);
+            for (int i = 0; i < choices.Count; i++)
+                Console.WriteLine("      " + choices[i]);
+
+            // 3. Get an input from the user and decides
+            do
+            {
+                // 4. Draw the arrow
+                Console.SetCursorPosition(3, topLine + userChoice);
+                Console.Write("->");
+
+                // 5. Get the user's input
+                userKey = Console.ReadKey(true);
+
+                // 6. Deletes the previous arrow
+                Console.SetCursorPosition(3, topLine + userChoice);
+                Console.Write("  ");
+
+                // 7. Choose what to do with the user's input
+                if (userKey.Key == ConsoleKey.DownArrow)
+                {
+                    userChoice++;
+                    // If exceed the number of choices, reset to the min choice
+                    if (userChoice > choices.Count)
+                        userChoice = 1;
+                }
+                else if (userKey.Key == ConsoleKey.UpArrow)
+                {
+                    userChoice--;
+                    // If is less than 1, set the choosed option to the last option
+                    if (userChoice < 1)
+                        userChoice = choices.Count;
+                }
+                else if (char.IsDigit(userKey.KeyChar))
+                    return userKey.KeyChar - '0';
+            } while (userKey.Key != ConsoleKey.Enter && userKey.Key != ConsoleKey.Spacebar);
+
+            // 8. Return the result
+            return userChoice;
         }
     }
 }
